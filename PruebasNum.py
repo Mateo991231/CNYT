a4b89cecb308abe5acc67855d9189c354903d194
unknown <mateo.quintero@labinfo.is.escuelaing.edu.co>
2020-01-30 10:57:46 -0500

    pruebasActualizadas

--- PruebasNum.py
+++ PruebasNum.py
@@ -1,10 +1,26 @@
 import unittest
-import complex
+import complexCal
 
-class TestStringMethods(unittest.TestCase):
-    ##
-    def test_deberiaSumarNumerosComplejos(self):
-        self.assertEquals(complex.suma(10,8,7,6),("18+13i"))
-
-if __name__=='__main__':
+class TestCases(unittest.TestCase):
+    def test_DeberiaSumar(self):
+        self.assertEqual(complexCal.suma(-3,7,7,-2),(4, 5))
+    def test_DeberiaRestar(self):
+        self.assertEqual(complexCal.resta(-3,7,7,-2),(-10,9))
+    def test_Deberiamultiplicar(self):
+        self.assertEqual(complexCal.multiplicacion(1,2,-1,-3),(-1,-5))
+    def test_deberiaHacerDivision(self):
+        self.assertEqual(complexCal.divi(10,20,30,5),(0.8235294117647058,1.2941176470588236))
+    def test_deberiaHacerConjugado(self):
+        self.assertEqual(complexCal.conjugado(10,-5),(10,5))
+    def test_deberiaHacerModulo(self):
+        self.assertEqual(complexCal.modulo(10,5),(11.180339887498949))
+    def test_deberiaPasarDePolarCartesiano(self):
+        self.assertEqual(complexCal.polarCartesiano((10,5)),(9.961946980917455, 0.8715574274765816))
+    def test_deberiaPasarDeCartesianopolar(self):
+       self.assertEqual(complexCal.cartesianoPolar((3,4)),(9765625, 53.13010235415598))
+    def test_deberiaRetornarFaseComplejo(self):
+       self.assertEqual(complexCal.fase((10,5)),0.4636476090008061)
+    
+if __name__=="__main__":
     unittest.main()
+
--- complexCal.py
+++ complexCal.py
@@ -1,44 +1,50 @@
 from sys import stdin
-
+import math
 def suma(realA,realB,imaA,imaB):
     respuestareal=0
     respuestaima=0
     respuestareal=realA+realB
     respuestaima=imaA+imaB
-    print(str(respuestareal)+"+"+str(respuestaima)+"i")
+    return (respuestareal,respuestaima)
 
 def resta(realA,realB,imaA,imaB):
     respuestareal=0
     respuestaima=0
     respuestareal=realA-realB
     respuestaima=imaA-imaB
-    print(str(respuestareal)+"-"+str(respuestaima)+"i")
+    return (respuestareal,respuestaima)
 
 def multiplicacion(realA,realB,imaA,imaB):
     parteuno=realA*realB-imaA*imaB
     partedos=realA*imaB+realB*imaA
-    print(str(parteuno)+"+"+str(partedos)+"i")
+    return (parteuno,partedos)
 
 def divi(realA,realB,imaA,imaB):
     parteuno=((realA*realB+imaA*imaB)/(realB**2+imaB**2))
     partedos=((realB*imaA-realA*imaB)/(realB**2+imaB**2))
-    print(str(parteuno)+"+"+str(partedos)+"i")
+    return (parteuno,partedos)
 
 def conjugado(real,imaginario):
     if imaginario<0 :
-        print(str(real)+"+"+str(abs(imaginario))+"i")
+        return (real,(abs(imaginario)))
     else:
-        print((str(real)+"-"+str(imaginario)+"i"))
+        return (real,imaginario)
 
 
 def modulo(real,imaginario):
     respuesta= ((real**2+imaginario**2)**0.5)
-    print(respuesta)
+    return (respuesta)
+
+def polarCartesiano(polar):
+    hipo=polar[0]
+    angu=polar[1]
+    return (hipo*math.cos(angu*(math.pi/180)),hipo*math.sin(angu*(math.pi/180)))
 
 
-def sumaVecto(vec1,vec2):
-    pass
-    
-    
-    
+def cartesianoPolar(cartesiano):
+    angulo=math.atan(cartesiano[1]/cartesiano[0])
+    return((cartesiano[0]**2+cartesiano[1]**2)**5,angulo*(180/math.pi))
 
+
+def fase(num):
+    return math.atan2(num[1],num[0])
