6d30b21fb232f6c61db8232efa82202ab0100a0d
unknown <mateo.quintero@labinfo.is.escuelaing.edu.co>
2020-02-20 11:17:49 -0500

    implementacion operaciones entre vec y mat

--- complexCal.py
+++ complexCal.py
@@ -89,4 +89,140 @@ def productoTensor (a,b):
                res.append(multiplicacion(a[i],b[j]))
           a[i]=res
      return a
+    
+def opuesto(a):
+    ar=a[0]*-1
+    br=a[1]*-1
+
+def inversoVector(vec):
+    respuesta=[]
+    for i in range(len(vec)):
+        res.append(opuesto(vec[i]))
+    return respuesta
+
+def inversaMatriz(mat):
+    respuesta=[]
+    for i in range(len(mat)):
+        aux=[]
+        for j in range(len(mat)):
+            res.append(opuesto(mat[i][j]))
+        respuesta.append(res)
+    return respuesta
+
+def sumaDeMatrices(mat1,mat2):
+    respuesta=[]
+    for i in range(len(mat1)):
+        aux=[]
+        for j in range(len(mat2)):
+            aux.append(suma(mat1[i][j],mat2[i][j]))
+        respuesta.append(aux)
+    return respuesta
+
+def escalarMat(mat1,mat2):
+    aux=[]
+    for i in range(len(mat1)):
+        respuesta=[]
+        for j in range(len(mat1)):
+            res.append(productoEscalar(mat1[i][j],mat2))
+    aux.append[res]
+    return aux
+
+def productoInternoVec(vec1,vec2):
+    respuesta = [0,0]
+    aux=[]
+    if len(vec1)==len(vec2):
+        for i in range(len(vec1)):
+           aux.append(multiplicacion(vec1[i],vec2[i]))
+           respuesta = suma(respuesta,aux[i])
+    return(res)
+def transpuesta(mat1):
+    respuesta=[]
+    for i in range(len(mat1[0])):
+        aux = []
+        for j in range(len(mat1)):
+            aux.append(mat1[j][i])
+        respuesta.append(aux)
+    return respuesta
+
+def conjugada(mat1):
+    respuesta=[]
+    for i in range(len(mat1)):
+        aux=[]
+        for j in range(len(mat1)):
+            aux.append(conjugado(mat1[i][j]))
+        respuesta.append(aux)
+    return respuesta
+
+
+def adjunta(a):
+    return (conjugada(transpuesta(mat1)))
+
+
+def multiMat(mat1,mat2):
+    respuesta = []
+    for i in range(len(mat1)):
+        aux1 = []
+        for j in range(len(mat2[0])):
+            aux2 = (0,0)
+            for k in range(len(mat2)):
+                res= multiplicacion(mat1[i][k],mat2[k][j])
+                aux2= suma(sumas,aux) 
+            aux1.append(aux2)
+        respuesta.append(aux1)
+    return (respuesta)
+
+def potencia(n):
+    return(multiplicacion(n,n))
+
+
+def distancia(vec1,vec2):
+    respuesta = []
+    solucion = [0,0]
+    if len(vec1)==len(vec2):
+        for i in range(len(vec1)):
+            vec1.append(resta(vec1[i],vec2[i]))
+        for j in range(len(respuesta)):
+            aux = suma(aux,potencia(respuesta[j]))
+    solucion[0]=solucion[0]**0.5
+    return (solucion)
+
+
+def norma(vec1):
+    res = [0,0]
+    for i in range(len(vec1)):
+        res = suma(solucion,potencia(vec1[i]))
+    res[0]=res[0]**0.5
+    return(res)
+
+
+def esUnaHermitiana(mat):
+    return mat == adjunta(mat)
+
+
+def esUnaUnitaria(mat):
+    array=[[(float(0),float(0)) for w in range(len(mat))]for j in range(len(mat))]
+    for k in range(len(array)):
+        array[k][k] = (float(1),float(0))
+    return multiMat(mat,adjunta(mat)) == multiMat(adjunta(mat),mat) == array
+
+
+
+def productoInternoMatrices(m1,m2):
+    adj = adjunta(m1)
+    aux = multiplicacionMatices(adj,m2)
+    res = (0,0)
+    for i in range(len (aux)):
+        res = suma(res,aux[i][i])
+    return modulo(res)
+
+def tensorMatrices(mat1,mat2):
+    respuesta=[]
+    for i in range(len(mat1)):
+        for j in range(len(mat2)):
+             sol.append(productoTensor(mat1[i],mat2[j]))
+    return sol
+
+
+def normaMatriz (mat):
+    return productoInternoMatrices(mat,mat)**0.5
 
