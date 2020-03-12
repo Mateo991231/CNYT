9896892c2cde403a9e5cf1bf8e4273850646c328
Mateo991231 <59961789+Mateo991231@users.noreply.github.com>
2020-03-12 15:59:32 -0500

    Update SimulacionTest.py

--- SimulacionTest.py
+++ SimulacionTest.py
@@ -3,8 +3,7 @@ class TestCases(unittest.TestCase):
     def test_probabilidad(self):
         self.assertEqual(complexCal.proba(2,[(7,5),(2,3),(1,2)]),15.48100641346688)
     def test_transicion(self):
-        self.assertEqual(complexCal.ampli(a=[(7,5),(5,2),(4,2)]
-                                          [(7,5),(2,3),(1,2)]),(98.0, -17.0))
+        self.assertEqual(complexCal.ampli(a=[(7,5),(5,2),(4,2)],[(7,5),(2,3),(1,2)]),(98.0, -17.0))
     
 if __name__=="__main__":
     unittest.main()
